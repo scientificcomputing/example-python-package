9a61cd79525f9f874e3d84053d6addf7a8c2ac78
jorgensd <dokken92@gmail.com>
2022-09-12 14:56:46 +0200

    Add coverage report

--- mypackage/functions.py
+++ mypackage/functions.py
@@ -2,9 +2,15 @@
 
 # Copyright (C) 2022 Jørgen Schartum Dokken
 #
-# This file is part of my_package
+# This file is part of mypackage
 # SPDX-License-Identifier:    MIT
 
 def addition(a: int, b: int) -> int:
     """ Computes a+b """
     return a + b
+
+
+def print_add(a: int, b: int) -> int:
+    c = a + b
+    print(c)
+    return c
