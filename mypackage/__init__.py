58c46379003a86f294543836a86bd1c68c2c52e3
Jørgen Schartum Dokken <dokken92@gmail.com>
2022-09-12 15:02:19 +0200

    Add initial structure to repository (#1)
    * Add actions/setup-python
    
    * More updates
    
    * Add coverage report

---
+++ mypackage/__init__.py
@@ -0,0 +1,9 @@
+
+# Copyright (C) 2022 Jørgen Schartum Dokken
+#
+# This file is part of my_package
+# SPDX-License-Identifier:    MIT
+
+from .functions import addition
+
+__all__ = ["addition"]
---
+++ mypackage/functions.py
@@ -0,0 +1,16 @@
+
+
+# Copyright (C) 2022 Jørgen Schartum Dokken
+#
+# This file is part of mypackage
+# SPDX-License-Identifier:    MIT
+
+def addition(a: int, b: int) -> int:
+    """ Computes a+b """
+    return a + b
+
+
+def print_add(a: int, b: int) -> int:
+    c = a + b
+    print(c)
+    return c
