c209bf1b3a056e68775fa477866d6ee55982d2dd
Jørgen Schartum Dokken <dokken92@gmail.com>
2022-09-13 13:57:36 +0200

    Change markdown templates to YAML. (#20)
    Add version to init.

--- src/mypackage/__init__.py
+++ src/mypackage/__init__.py
@@ -4,6 +4,11 @@
 # This file is part of my_package
 # SPDX-License-Identifier:    MIT
 
+import importlib.metadata
+
 from .functions import addition, print_add
+
+__version__ = importlib.metadata.version(__package__)
+
 
 __all__ = ["addition", "print_add"]
