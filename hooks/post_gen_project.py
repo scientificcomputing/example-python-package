0d46304a4e9a46a22b6a5cbe4941d8d8e9b9a2c8
jorgensd <dokken92@gmail.com>
2022-10-11 16:00:45 +0200

    Add workflows file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,9 @@ import os
 REMOVE_PATHS = [
     '{% if cookiecutter.testing|lower != "y" %} tests/test_import.py{% endif %}',
     '{% if cookiecutter.testing|lower != "y" %} tests/{% endif %}',
+    '{% if cookiecutter.testing|lower != "y" %} .github/workflows/test_package.yml{% endif %}',
+    '{% if cookiecutter.linting|lower != "y" %} .github/workflows/test_formatting.yml{% endif %}',
+    '{% if cookiecutter.linting|lower != "y" %} .flake8{% endif %}'
 ]
 
 for path in REMOVE_PATHS:
